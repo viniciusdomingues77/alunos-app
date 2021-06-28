d2ea3b18671b568d7e5b78efe91de4604c20d05a
Vinicius da Silva Domingues <i.niv2010@hotmail.com>
2021-06-28 20:55:09 -0300

    agenda do aluno desmarcando horario

--- src/agendasdoaluno.js
+++ src/agendasdoaluno.js
@@ -1,126 +1,214 @@
-import * as React from "react";
-import { DataGrid, ptBR } from "@material-ui/data-grid";
-import { usePromiseTracker, trackPromise } from "react-promise-tracker";
-import Avatar from "@material-ui/core/Avatar";
-import Grid from "@material-ui/core/Grid";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import IconButton from "@material-ui/core/IconButton";
-import EventBusyIcon from "@material-ui/icons/EventBusy";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import Button from "@material-ui/core/Button";
-
-const useStyles = makeStyles((theme) => ({
+import * as React from 'react'
+import { DataGrid, ptBR } from '@material-ui/data-grid'
+import { usePromiseTracker, trackPromise } from 'react-promise-tracker'
+import Avatar from '@material-ui/core/Avatar'
+import Grid from '@material-ui/core/Grid'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import IconButton from '@material-ui/core/IconButton'
+import EventBusyIcon from '@material-ui/icons/EventBusy'
+import Dialog from '@material-ui/core/Dialog'
+import DialogActions from '@material-ui/core/DialogActions'
+import DialogContent from '@material-ui/core/DialogContent'
+import DialogContentText from '@material-ui/core/DialogContentText'
+import DialogTitle from '@material-ui/core/DialogTitle'
+import Button from '@material-ui/core/Button'
+import RadioGroup from '@material-ui/core/RadioGroup'
+import Backdrop from '@material-ui/core/Backdrop'
+import Fade from '@material-ui/core/Fade'
+import Radio from '@material-ui/core/Radio'
+import Modal from '@material-ui/core/Modal'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import BarraProgressoFixa from './barraprogressofixa'
+import Typography from '@material-ui/core/Typography'
+import Snackbar from '@material-ui/core/Snackbar'
+import { Alert } from '@material-ui/lab'
+const useStyles = makeStyles(theme => ({
   avatar: {
     width: theme.spacing(6),
     height: theme.spacing(6),
-    top: 0,
+    top: 0
   },
-}));
+  modal: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: '3px solid #306898',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3)
+  },
+  cabecalho: {
+    width: '100%',
+    height: '10px'
+  }
+}))
 
-export default function HistoricoAgendasAluno(props) {
-  const [AgendasAluno, setAgendasAluno] = React.useState([]);
-  const [FotoAluno, setFotoAluno] = React.useState([]);
-  const [openError, setOpenError] = React.useState(false);
-  const [select, setSelection] = React.useState([]);
-  const [textoExc, settextoExc] = React.useState("");
-  const classes = useStyles();
+export default function HistoricoAgendasAluno (props) {
+  const [AgendasAluno, setAgendasAluno] = React.useState([])
+  const [FotoAluno, setFotoAluno] = React.useState([])
+  const [openError, setOpenError] = React.useState(false)
+  const [select, setSelection] = React.useState('')
+  const [textoExc, settextoExc] = React.useState('')
+  const { promiseInProgress } = usePromiseTracker()
+  const [Cancelando, setCancelando] = React.useState(false)
+  const [valueCancelamento, setValueCancelamento] = React.useState('')
+  const [openModal, setopenModal] = React.useState(false)
+  const [TextoBarraProgresso, setTextoBarraProgresso] = React.useState('')
+
+  const classes = useStyles()
   const columns = [
-    { field: "id", headerName: "ID", width: 90 },
-    { field: "strdata", headerName: "Data", width: 110, type: "date" },
-    { field: "strhora", headerName: "Hora", width: 110 },
-    { field: "diasemana", headerName: "Dia", width: 100 },
-  ];
-  const [openDialogoExc, setopenDialogoExc] = React.useState(false);
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'strdata', headerName: 'Data', width: 110, type: 'date' },
+    { field: 'strhora', headerName: 'Hora', width: 110 },
+    { field: 'diasemana', headerName: 'Dia', width: 100 }
+  ]
+  const [openDialogoExc, setopenDialogoExc] = React.useState(false)
+  const handleRadioChangeCancelamento = event => {
+    // setvalueAgendaCancelada(false)
+    setValueCancelamento(event.target.value)
+  }
   const handleClickCancelaAgenda = () => {
-    setopenDialogoExc(true);
-    settextoExc(
-      "Todas as agendas selecionadas e seus respectivos prontuários serão deletados. Confirma?"
-    );
-  };
+    var idsstr = select.toString()
+    if (!idsstr) {
+      return
+    }
+    if (idsstr.length == 0) {
+      return
+    }
+    var ids = idsstr.split(',')
+    setopenModal(true)
+    setValueCancelamento('professor')
+  }
+  const handleCloseModal = (event, reason) => {
+    setopenModal(false)
+  }
   const handleCloseExc = (event, reason) => {
-    console.log("selecionados  " + select);
-    setopenDialogoExc(false);
-  };
+    console.log('selecionados  ' + select)
+    setopenDialogoExc(false)
+  }
 
-  const handleClickDelete = () => {
+  const handleClickCancelaAgendaConfirmacao = () => {
+    var idsstr = select.toString()
+    var ids = idsstr.split(',')
+    setopenModal(false)
     const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
-        AgendasdoAluno: select,
-      }),
-    };
+        idagendas: ids,
+        stropcao: valueCancelamento
+      })
+    }
+    setTextoBarraProgresso('Desmarcando')
     trackPromise(
-      fetch("https://localhost:44363/api/agenda/exclusaorange", requestOptions)
-        .then((response) => {
+      fetch('https://localhost:44363/api/agenda/exclusaorange', requestOptions)
+        .then(response => {
           if (!response.ok) {
-            throw Error(response.statusText);
+            throw Error(response.statusText)
           }
-          return response;
+          return response
         })
-        .then((response) => response.json())
-        .then((d) => console.log(d))
+        .then(response => response.json())
+        .then(d => setCancelando(true))
         .catch(function (error) {
-          setOpenError(true);
+          setOpenError(true)
+          setCancelando(false)
         })
-    );
-  };
-
+    )
+  }
+  const handleCloseError = (event, reason) => {
+    if (reason === 'clickaway') {
+      return
+    }
+    setOpenError(false)
+  }
   React.useEffect(() => {
+    setTextoBarraProgresso('Carregando')
     const apiUrla =
-      `https://localhost:44363/api/agenda/todasasagendas/` + props.idaluno;
+      `https://localhost:44363/api/agenda/todasasagendas/` + props.idaluno
     trackPromise(
       fetch(apiUrla)
-        .then((response) => {
+        .then(response => {
           if (!response.ok) {
-            throw Error(response.statusText);
+            throw Error(response.statusText)
           }
-          return response;
+          return response
         })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          setAgendasAluno(data);
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          setAgendasAluno(data)
         })
         .catch(function (error) {
-          console.log("catch error" + error);
-          setOpenError(true);
+          console.log('catch error' + error)
+          setOpenError(true)
         })
-    );
-    const apiUrlf = `https://localhost:44363/api/aluno/foto/` + props.idaluno;
+    )
+    const apiUrlf = `https://localhost:44363/api/aluno/foto/` + props.idaluno
     trackPromise(
       fetch(apiUrlf)
-        .then((response) => {
+        .then(response => {
           if (!response.ok) {
-            throw Error(response.statusText);
+            throw Error(response.statusText)
           }
-          return response;
+          return response
         })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          setFotoAluno(data);
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          setFotoAluno(data)
         })
         .catch(function (error) {
-          console.log("catch error" + error);
-          setOpenError(true);
+          console.log('catch error' + error)
+          setOpenError(true)
         })
-    );
-  }, []);
+    )
+  }, [])
+
+  React.useEffect(() => {
+    if (Cancelando) {
+      setTextoBarraProgresso('Carregando agendas')
+      const apiUrla =
+        `https://localhost:44363/api/agenda/todasasagendas/` + props.idaluno
+      trackPromise(
+        fetch(apiUrla)
+          .then(response => {
+            if (!response.ok) {
+              throw Error(response.statusText)
+            }
+            return response
+          })
+          .then(res => res.json())
+          .then(data => {
+            console.log(data)
+            setAgendasAluno(data)
+          })
+          .catch(function (error) {
+            console.log('catch error' + error)
+            setOpenError(true)
+          })
+      )
+      setCancelando(false)
+    }
+  }, [Cancelando])
 
   return (
     <React.Fragment>
       <Grid container spacing={3}>
+        <div className={classes.cabecalho}>
+          <BarraProgressoFixa
+            titulo={TextoBarraProgresso}
+            loading={promiseInProgress}
+          />
+        </div>
         <Grid
           item
           xs={2}
           style={{
-            display: "flex",
-            justifyContent: "flex-end",
+            display: 'flex',
+            justifyContent: 'flex-end'
           }}
         >
           <Avatar className={classes.avatar} src={FotoAluno.foto}></Avatar>
@@ -129,26 +217,26 @@ export default function HistoricoAgendasAluno(props) {
           item
           xs={8}
           style={{
-            display: "flex",
-            justifyContent: "flex-start",
-            alignItems: "center",
+            display: 'flex',
+            justifyContent: 'flex-start',
+            alignItems: 'center'
           }}
         >
           {FotoAluno.nome}
         </Grid>
         <Grid
           item
-          xs={2}
+          xs={1}
           style={{
-            display: "flex",
-            justifyContent: "flex-start",
-            alignItems: "center",
+            display: 'flex',
+            justifyContent: 'flex-start',
+            alignItems: 'center'
           }}
         >
           <IconButton
-            color="primary"
-            aria-label="Remove Agenda"
-            component="span"
+            color='primary'
+            aria-label='Remove Agenda'
+            component='span'
             onClick={handleClickCancelaAgenda}
           >
             <EventBusyIcon />
@@ -158,45 +246,94 @@ export default function HistoricoAgendasAluno(props) {
 
       <Grid container spacing={3}>
         <Grid item xs={12} style={{ marginLeft: 0 }}>
-          <div style={{ height: 400, width: "600px" }}>
+          <div style={{ height: 400, width: '600px' }}>
             <DataGrid
               rows={AgendasAluno}
-              columns={columns.map((column) => ({
+              columns={columns.map(column => ({
                 ...column,
-                sortable: false,
+                sortable: false
               }))}
               pageSize={5}
               checkboxSelection
               localeText={ptBR.props.MuiDataGrid.localeText}
-              onSelectionModelChange={(e) => {
-                console.log("e " + e.selectionModel);
-                //setSelection(newSelection);
+              onSelectionModelChange={e => {
+                setSelection(e.selectionModel)
               }}
             />
           </div>
         </Grid>
       </Grid>
-      <Dialog
-        open={openDialogoExc}
-        onClose={handleCloseExc}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
+
+      <Modal
+        aria-labelledby='transition-modal-title'
+        aria-describedby='transition-modal-description'
+        className={classes.modal}
+        open={openModal}
+        onClose={handleCloseModal}
+        closeAfterTransition
+        BackdropComponent={Backdrop}
+        BackdropProps={{
+          timeout: 500
+        }}
       >
-        <DialogTitle id="alert-dialog-title">{"Atenção"}</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            {textoExc}
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseExc} color="primary">
-            Desistir
-          </Button>
-          <Button onClick={handleClickDelete} color="primary">
-            Confirmar
-          </Button>
-        </DialogActions>
-      </Dialog>
+        <Fade in={openModal}>
+          <div className={classes.paper}>
+            <h2 id='transition-modal-title'>Desmarcação</h2>
+            <p id='transition-modal-description'>
+              <RadioGroup
+                aria-label='quiz'
+                name='quiz'
+                value={valueCancelamento}
+                onChange={handleRadioChangeCancelamento}
+              >
+                <FormControlLabel
+                  value='professor'
+                  control={<Radio />}
+                  label='Desmarcado pelo professor'
+                />
+                <FormControlLabel
+                  value='aluno'
+                  control={<Radio />}
+                  label='Desmarcado pelo aluno'
+                />
+              </RadioGroup>
+            </p>
+            <Grid container spacing={3}>
+              <Grid
+                item
+                xs={6}
+                style={{ display: 'flex', justifyContent: 'flex-start' }}
+              >
+                <Button
+                  variant='outlined'
+                  color='secondary'
+                  onClick={() => handleClickCancelaAgendaConfirmacao()}
+                >
+                  Desmarcar
+                </Button>
+              </Grid>
+              <Grid
+                item
+                xs={6}
+                style={{ display: 'flex', justifyContent: 'flex-end' }}
+              >
+                <Button variant='contained' onClick={() => setopenModal(false)}>
+                  Fechar
+                </Button>
+              </Grid>
+            </Grid>
+          </div>
+        </Fade>
+      </Modal>
+      <Snackbar
+        open={openError}
+        autoHideDuration={6000}
+        onClose={handleCloseError}
+      >
+        <Alert onClose={handleCloseError} severity='error'>
+          Não foi possível realizar a operação. Contacte o desenvolvedor
+        </Alert>
+      </Snackbar>
     </React.Fragment>
-  );
+  )
 }
--- src/barraprogressofixa.js
+++ src/barraprogressofixa.js
@@ -1,25 +1,28 @@
-import React from "react";
-import LinearProgress from "@material-ui/core/LinearProgress";
-import Grid from "@material-ui/core/Grid";
-import { makeStyles, useTheme } from "@material-ui/core/styles";
-import "fontsource-roboto";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-const useStyles = makeStyles((theme) => ({
+import React from 'react'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Grid from '@material-ui/core/Grid'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import 'fontsource-roboto'
+import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
+const useStyles = makeStyles(theme => ({
   root: {
     flexShrink: 0,
-    marginLeft: theme.spacing(2.5),
+    marginLeft: theme.spacing(2.5)
   },
   titulobarra: {
     margin: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   linha: {
-    backgroundColor: "#3F51B5",
-    height: "3px",
-  },
-}));
-export default function BarraProgressoFixa(props) {
-  const classes = useStyles();
+    backgroundColor: '#3F51B5',
+    height: '3px'
+  }
+}))
+export default function BarraProgressoFixa (props) {
+  const classes = useStyles()
   return (
     <React.Fragment>
       <Grid container spacing={0}>
@@ -28,11 +31,11 @@ export default function BarraProgressoFixa(props) {
           {!props.loading && <Divider className={classes.linha} />}
         </Grid>
         <Grid item xs={12} className={classes.titulobarra}>
-          <Typography variant="caption" display="block" gutterBottom>
+          <Typography variant='caption' display='block' gutterBottom>
             {props.loading && props.titulo}
           </Typography>
         </Grid>
       </Grid>
     </React.Fragment>
-  );
+  )
 }
