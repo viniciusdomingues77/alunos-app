33c47598690d7ec99b84b59a7fc88de8982cc0d6
Vinicius da Silva Domingues <i.niv2010@hotmail.com>
2021-06-30 19:51:11 -0300

    ajusate no layout do prontuário, lista de professor carregada no prontuário

--- src/gradedatahorario.js
+++ src/gradedatahorario.js
@@ -1,11 +1,11 @@
-import React from "react";
-import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
-import { makeStyles } from "@material-ui/core/styles";
-import { useSelector, useDispatch } from "react-redux";
-import { usePromiseTracker, trackPromise } from "react-promise-tracker";
+import React from 'react'
+import Box from '@material-ui/core/Box'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+import { useSelector, useDispatch } from 'react-redux'
+import { usePromiseTracker, trackPromise } from 'react-promise-tracker'
 
-import Typography from "@material-ui/core/Typography";
+import Typography from '@material-ui/core/Typography'
 import {
   action,
   AppState,
@@ -13,125 +13,125 @@ import {
   SetIDAgendaSelProntuarioAction,
   SetIDAlunoSelProntuarioAction,
   SetDataSelProntuarioAction,
-  SetFotoSelProntuarioAction,
-} from "./ConfigSate";
-import { isClassExpression } from "typescript";
+  SetFotoSelProntuarioAction
+} from './ConfigSate'
+import { isClassExpression } from 'typescript'
 const useStyles = makeStyles({
   root: {
     boxShadow:
-      "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
-    padding: "5px",
-    width: "100%",
-    height: "250px",
-    overflow: "auto",
+      '0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)',
+    padding: '5px',
+    width: '100%',
+    height: '250px',
+    overflow: 'auto',
+    backgroundColor: '#F5F5F5'
   },
   boxcalendar: {
-    color: "white",
-    fontSize: "15px",
+    color: 'white',
+    fontSize: '15px'
     // borderStyle: "solid",
     // borderWidth: "thin",
     // borderColor: "#3c52b2",
   },
   btncalendar: {
-    background: "#306898",
-    borderRadius: "3px",
-
-    fontSize: "11px",
-    border: "0",
-    color: "white",
-    padding: "0 10px",
-    "&:hover": {
-      backgroundColor: "#fff",
-      color: "#3c52b2",
-    },
+    background: '#F5F5F5',
+    borderRadius: '3px',
+    borderColor: '#306898',
+    fontSize: '11px',
+    color: '#306898',
+    padding: '0 10px',
+    '&:hover': {
+      backgroundColor: '#fff',
+      color: '#3c52b2'
+    }
   },
   divCabecalhoCalendario: {
-    background: "white",
-    width: "100%",
-  },
-});
+    background: '#F5F5F5',
+    width: '100%'
+  }
+})
 
-export default function AgendasAluno() {
-  const classes = useStyles();
-  const [Agendas, setAgendas] = React.useState([]);
-  const { promiseInProgress } = usePromiseTracker();
-  const [TextoBarraProgresso, setTextoBarraProgresso] = React.useState("");
+export default function AgendasAluno () {
+  const classes = useStyles()
+  const [Agendas, setAgendas] = React.useState([])
+  const { promiseInProgress } = usePromiseTracker()
+  const [TextoBarraProgresso, setTextoBarraProgresso] = React.useState('')
   const idagenda = useSelector(
-    (state) => state.configuracoes.IDAgendaSelProntuario
-  );
-  const idaluno = useSelector(
-    (state) => state.configuracoes.IDAlunoSelProntuario
-  );
-  const dispatch = useDispatch();
+    state => state.configuracoes.IDAgendaSelProntuario
+  )
+  const idaluno = useSelector(state => state.configuracoes.IDAlunoSelProntuario)
+  const dispatch = useDispatch()
   const SelIdAgenda = (idagenda, data) => {
-    dispatch(SetIDAgendaSelProntuarioAction(idagenda));
-    dispatch(SetDataSelProntuarioAction(new Date(data)));
-  };
+    dispatch(SetIDAgendaSelProntuarioAction(idagenda))
+    dispatch(SetDataSelProntuarioAction(new Date(data)))
+  }
 
   React.useEffect(() => {
-    const apiUrl = `https://localhost:44363/api/agenda/agendas/` + idaluno;
+    const apiUrl = `https://localhost:44363/api/agenda/agendas/` + idaluno
     trackPromise(
       fetch(apiUrl)
-        .then((response) => {
+        .then(response => {
           if (!response.ok) {
-            throw Error(response.statusText);
+            throw Error(response.statusText)
           }
-          return response;
+          return response
         })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          setAgendas(data);
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          setAgendas(data)
         })
         .catch(function (error) {
-          console.log("catch error" + error);
+          console.log('catch error' + error)
           //setOpenError(true);
         })
-    );
-    const apiUrl2 = `https://localhost:44363/api/aluno/foto/` + idaluno;
+    )
+    const apiUrl2 = `https://localhost:44363/api/aluno/foto/` + idaluno
     trackPromise(
       fetch(apiUrl2)
-        .then((response) => {
+        .then(response => {
           if (!response.ok) {
-            throw Error(response.statusText);
+            throw Error(response.statusText)
           }
-          return response;
+          return response
         })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          dispatch(SetFotoSelProntuarioAction(data.foto));
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          dispatch(SetFotoSelProntuarioAction(data.foto))
         })
         .catch(function (error) {
-          console.log("catch error" + error);
+          console.log('catch error' + error)
           //setOpenError(true);
         })
-    );
-  }, [idaluno]);
+    )
+  }, [idaluno])
 
   return (
     <div className={classes.root}>
       <div className={classes.divCabecalhoCalendario}>
-        <Typography variant="overline" style={{ color: "#306898" }}>
+        <Typography variant='overline' style={{ color: '#306898' }}>
           Agendas do aluno
         </Typography>
       </div>
 
       <Box
-        flexWrap="wrap"
-        display="flex"
-        flexDirection="row"
-        justifyContent="flex-start"
+        flexWrap='wrap'
+        display='flex'
+        flexDirection='row'
+        justifyContent='flex-start'
         p={0}
         m={0}
-        bgcolor="background.paper"
+        bgcolor='#F5F5F5'
       >
         {/* {idaluno == 0 && <Typography variant="h5">Agenda do aluno</Typography>} */}
 
-        {Agendas.map((agenda) => (
+        {Agendas.map(agenda => (
           <Box
             className={classes.boxcalendar}
-            style={{ marginTop: "2px", marginRight: "2px" }}
+            border={1}
+            borderColor='grey.500'
+            style={{ marginTop: '2px', marginRight: '2px' }}
           >
             <Button
               className={classes.btncalendar}
@@ -145,5 +145,5 @@ export default function AgendasAluno() {
         ))}
       </Box>
     </div>
-  );
+  )
 }
--- src/prontuariocadastro.js
+++ src/prontuariocadastro.js
@@ -1,254 +1,276 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import AgendasAluno from "./gradedatahorario";
-import Grid from "@material-ui/core/Grid";
-import Autocomplete from "@material-ui/lab/Autocomplete";
-import TextField from "@material-ui/core/TextField";
-import { useSelector, useDispatch } from "react-redux";
-import { usePromiseTracker, trackPromise } from "react-promise-tracker";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import Avatar from "@material-ui/core/Avatar";
-import CancelPresentationIcon from "@material-ui/icons/CancelPresentation";
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import AgendasAluno from './gradedatahorario'
+import Grid from '@material-ui/core/Grid'
+import Autocomplete from '@material-ui/lab/Autocomplete'
+import TextField from '@material-ui/core/TextField'
+import { useSelector, useDispatch } from 'react-redux'
+import { usePromiseTracker, trackPromise } from 'react-promise-tracker'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import Avatar from '@material-ui/core/Avatar'
+import CancelPresentationIcon from '@material-ui/icons/CancelPresentation'
 import {
   action,
   AppState,
   store,
-  SetIDAlunoSelProntuarioAction,
-} from "./ConfigSate";
-import ReactDOM from "react-dom";
-import { EditorState, convertToRaw, convertFromRaw } from "draft-js";
-import { Editor } from "react-draft-wysiwyg";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import SaveIcon from "@material-ui/icons/Save";
-import BarraProgressoFixa from "./barraprogressofixa";
-const useStyles = makeStyles((theme) => ({
+  SetIDAlunoSelProntuarioAction
+} from './ConfigSate'
+import ReactDOM from 'react-dom'
+import { EditorState, convertToRaw, convertFromRaw } from 'draft-js'
+import { Editor } from 'react-draft-wysiwyg'
+import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
+import SaveIcon from '@material-ui/icons/Save'
+import BarraProgressoFixa from './barraprogressofixa'
+const useStyles = makeStyles(theme => ({
   root: {
-    display: "flex",
-    flexWrap: "wrap",
-    justifyContent: "space-around",
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
     marginTop: 30,
-    backgroundColor: theme.palette.background.paper,
+    backgroundColor: theme.palette.background.paper
   },
   gridList: {
     width: 500,
-    height: 450,
+    height: 450
   },
   icon: {
-    color: "rgba(255, 255, 255, 0.54)",
+    color: 'rgba(255, 255, 255, 0.54)'
   },
   texteditor: {
-    backgroundColor: "#306898",
+    backgroundColor: '#306898',
 
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "center",
-    fontSize: "12px",
-    color: "white",
-    marginBottom: "5px",
-    textAlign: "center",
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: '12px',
+    color: 'white',
+    marginBottom: '5px',
+    textAlign: 'center'
   },
   wrapperclass: {
-    padding: "1rem",
-    border: "1px solid #ccc",
+    padding: '1rem',
+    border: '1px solid #ccc'
   },
   editorclass: {
-    backgroundColor: "#ECF0F1",
-    padding: "1rem",
-    border: "1px solid #306898s",
-    height: "300px",
+    backgroundColor: '#ECF0F1',
+    padding: '1rem',
+    border: '1px solid #306898s',
+    height: '300px'
   },
   toolbarclass: {
-    border: "1px solid #ccc",
+    border: '1px solid #ccc'
   },
   cabecalho: {
-    with: "100%",
-    height: "10px",
+    with: '100%',
+    height: '10px'
   },
   avatar: {
     width: theme.spacing(12),
-    height: theme.spacing(12),
+    height: theme.spacing(12)
   },
-  divEditor: {},
-}));
+  divEditor: {}
+}))
 
-export default function ProntuarioCadastro() {
-  const classes = useStyles();
-  const [Alunos, setAlunos] = React.useState([]);
-  const [Aluno, setAluno] = React.useState("");
-  const { promiseInProgress } = usePromiseTracker();
-  const [TextoBarraProgresso, setTextoBarraProgresso] = React.useState("");
-  const [dataExt, setdataExt] = React.useState("");
+export default function ProntuarioCadastro () {
+  const [professores, setProfessores] = React.useState([])
+  const [Professor, setProfessor] = React.useState('')
+  const classes = useStyles()
+  const [Alunos, setAlunos] = React.useState([])
+  const [Aluno, setAluno] = React.useState('')
+  const { promiseInProgress } = usePromiseTracker()
+  const [TextoBarraProgresso, setTextoBarraProgresso] = React.useState('')
+  const [dataExt, setdataExt] = React.useState('')
+  const [openError, setOpenError] = React.useState(false)
   const [editorState, setEditorState] = React.useState(() =>
     EditorState.createEmpty()
-  );
+  )
   const idalunosel = useSelector(
-    (state) => state.configuracoes.IDAlunoSelProntuario
-  );
+    state => state.configuracoes.IDAlunoSelProntuario
+  )
 
   const idagendasel = useSelector(
-    (state) => state.configuracoes.IDAgendaSelProntuario
-  );
+    state => state.configuracoes.IDAgendaSelProntuario
+  )
 
   const fotoalunosel = useSelector(
-    (state) => state.configuracoes.FotoAlunoSelProntuario
-  );
+    state => state.configuracoes.FotoAlunoSelProntuario
+  )
 
-  const datasel = useSelector((state) => state.configuracoes.DataSelProntuario);
+  const datasel = useSelector(state => state.configuracoes.DataSelProntuario)
 
-  function MesporExtenso(nummes) {
+  function MesporExtenso (nummes) {
     switch (nummes) {
       case 0:
-        return "Janeiro";
-        break;
+        return 'Janeiro'
+        break
       case 1:
-        return "Fevereiro";
-        break;
+        return 'Fevereiro'
+        break
       case 2:
-        return "Março";
-        break;
+        return 'Março'
+        break
       case 3:
-        return "Abril";
-        break;
+        return 'Abril'
+        break
       case 4:
-        return "Maio";
-        break;
+        return 'Maio'
+        break
       case 5:
-        return "Junho";
-        break;
+        return 'Junho'
+        break
       case 6:
-        return "Julho";
-        break;
+        return 'Julho'
+        break
       case 7:
-        return "Agosto";
-        break;
+        return 'Agosto'
+        break
       case 8:
-        return "Setembro";
-        break;
+        return 'Setembro'
+        break
       case 9:
-        return "Outubro";
-        break;
+        return 'Outubro'
+        break
       case 10:
-        return "Novembro";
-        break;
+        return 'Novembro'
+        break
       case 11:
-        return "Dezembro";
-        break;
+        return 'Dezembro'
+        break
 
       default:
     }
   }
 
-  function DataporExtenso(data) {
+  function DataporExtenso (data) {
     var dt =
       data.getDate() +
-      " de " +
+      ' de ' +
       MesporExtenso(data.getMonth()) +
-      " de " +
-      data.getFullYear();
-    return dt;
+      ' de ' +
+      data.getFullYear()
+    return dt
   }
-  const dispatch = useDispatch();
-  const SelIdAluno = (idaluno) => {
-    setTextoBarraProgresso("Carregando agendas do aluno");
-    dispatch(SetIDAlunoSelProntuarioAction(idaluno));
-  };
+  const dispatch = useDispatch()
+  const SelIdAluno = idaluno => {
+    setTextoBarraProgresso('Carregando agendas do aluno')
+    dispatch(SetIDAlunoSelProntuarioAction(idaluno))
+  }
   const SalvarProntuario = () => {
-    var texto = JSON.stringify(convertToRaw(editorState.getCurrentContent()));
+    var texto = JSON.stringify(convertToRaw(editorState.getCurrentContent()))
 
     const requestOptions = {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         idagenda: idagendasel,
-        texto: texto,
-      }),
-    };
-    setTextoBarraProgresso("Salvando prontuário");
+        texto: texto
+      })
+    }
+    setTextoBarraProgresso('Salvando prontuário')
     trackPromise(
-      fetch("https://localhost:44363/api/prontuario/", requestOptions)
-        .then((response) => {
+      fetch('https://localhost:44363/api/prontuario/', requestOptions)
+        .then(response => {
           if (!response.ok) {
-            throw Error(response.statusText);
+            throw Error(response.statusText)
           }
-          return response;
+          return response
         })
-        .then((response) => response.json())
-        .then((d) => console.log("data res " + d))
+        .then(response => response.json())
+        .then(d => console.log('data res ' + d))
         .catch(function (error) {
-          console.log("catch error" + error);
+          console.log('catch error' + error)
           //setSubmitSuccess(false);
           //setOpenError(true);
         })
-    );
-  };
+    )
+  }
 
   React.useEffect(() => {
-    setTextoBarraProgresso("Listando alunos");
-    const apiUrl = `https://localhost:44363/api/aluno/identificacao`;
+    setTextoBarraProgresso('Listando alunos')
+    const apiUrl = `https://localhost:44363/api/aluno/identificacao`
     trackPromise(
       fetch(apiUrl)
-        .then((response) => {
+        .then(response => {
           if (!response.ok) {
-            throw Error(response.statusText);
+            throw Error(response.statusText)
           }
-          return response;
+          return response
         })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          setAlunos(data);
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          setAlunos(data)
         })
         .catch(function (error) {
-          console.log("catch error" + error);
+          console.log('catch error' + error)
           //setOpenError(true);
         })
-    );
-  }, []);
+    )
+    const apiUrlp = `https://localhost:44363/api/professor`
+    trackPromise(
+      fetch(apiUrlp)
+        .then(response => {
+          if (!response.ok) {
+            throw Error(response.statusText)
+          }
+          return response
+        })
+        .then(res => res.json())
+        .then(data => {
+          console.log(data)
+          setProfessores(data)
+        })
+        .catch(function (error) {
+          console.log('catch error' + error)
+          setOpenError(true)
+        })
+    )
+  }, [])
 
   React.useEffect(() => {
     if (idagendasel == 0) {
-      return;
+      return
     }
 
-    setTextoBarraProgresso("Carregando prontuário");
+    setTextoBarraProgresso('Carregando prontuário')
     const apiUrl2 =
-      `https://localhost:44363/api/prontuario?idagenda=` + idagendasel;
+      `https://localhost:44363/api/prontuario?idagenda=` + idagendasel
     trackPromise(
       fetch(apiUrl2)
-        .then((response) => {
+        .then(response => {
           if (!response.ok) {
-            throw Error(response.statusText);
+            throw Error(response.statusText)
           }
 
-          return response;
+          return response
         })
-        .then((res) => res.json())
-        .then((data) => {
+        .then(res => res.json())
+        .then(data => {
           if (data) {
             if (data.texto) {
               setEditorState(
                 EditorState.createWithContent(
                   convertFromRaw(JSON.parse(data.texto))
                 )
-              );
+              )
             } else {
-              setEditorState(EditorState.createEmpty());
+              setEditorState(EditorState.createEmpty())
             }
           } else {
-            setEditorState(EditorState.createEmpty());
+            setEditorState(EditorState.createEmpty())
           }
-          setdataExt(DataporExtenso(datasel));
+          setdataExt(DataporExtenso(datasel))
         })
         .catch(function (error) {
-          console.log("catch error" + error);
-          setEditorState(EditorState.createEmpty());
+          console.log('catch error' + error)
+          setEditorState(EditorState.createEmpty())
           //setOpenError(true);
         })
-    );
-  }, [idagendasel]);
-  function ClearFields() {}
+    )
+  }, [idagendasel])
+  function ClearFields () {}
   return (
     <React.Fragment>
       <div className={classes.cabecalho}>
@@ -260,42 +282,69 @@ export default function ProntuarioCadastro() {
       <div className={classes.root}>
         <Grid container spacing={3}>
           <Grid item xs={7}>
-            <Grid container spacing={3} direction="column">
+            <Grid container spacing={3} direction='column'>
               <Grid item xs={12}>
                 <Autocomplete
-                  value={Aluno}
-                  id="autocomplete"
+                  value={Professor}
+                  id='autocomplete'
                   onChange={(event, newValue) => {
-                    setAluno(newValue);
-                    console.log("aluno on change " + newValue);
-                    var idaluno = "0";
+                    setProfessor(newValue)
                     if (newValue) {
-                      idaluno = newValue
-                        .substring(0, newValue.indexOf("-"))
-                        .trim();
                     }
-                    SelIdAluno(idaluno);
                   }}
-                  options={Alunos.map(
-                    (aluno) => `${aluno.idaluno} - ${aluno.nome}`
+                  options={professores.map(
+                    professor => `${professor.idprofessor} - ${professor.nome}`
                   )}
                   getOptionSelected={(option, value) => {
-                    return option === value;
+                    return option === value
                   }}
-                  style={{ width: "100%", marginBottom: 2 }}
-                  renderInput={(params) => (
+                  style={{ width: '100%', marginBottom: 5 }}
+                  renderInput={params => (
                     <TextField
                       {...params}
-                      label="Aluno"
+                      label='Professor'
                       required
                       autoFocus
-                      variant="outlined"
+                      variant='outlined'
                     />
                   )}
                 />
               </Grid>
               <Grid item xs={12}>
-                <Avatar className={classes.avatar} src={fotoalunosel}></Avatar>
+                <Autocomplete
+                  value={Aluno}
+                  id='autocomplete'
+                  onChange={(event, newValue) => {
+                    setAluno(newValue)
+                    console.log('aluno on change ' + newValue)
+                    var idaluno = '0'
+                    if (newValue) {
+                      idaluno = newValue
+                        .substring(0, newValue.indexOf('-'))
+                        .trim()
+                    }
+                    SelIdAluno(idaluno)
+                  }}
+                  options={Alunos.map(
+                    aluno => `${aluno.idaluno} - ${aluno.nome}`
+                  )}
+                  getOptionSelected={(option, value) => {
+                    return option === value
+                  }}
+                  style={{ width: '100%', marginBottom: 2 }}
+                  renderInput={params => (
+                    <TextField
+                      {...params}
+                      label='Aluno'
+                      required
+                      autoFocus
+                      variant='outlined'
+                    />
+                  )}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                {/* <Avatar className={classes.avatar} src={fotoalunosel}></Avatar> */}
               </Grid>
             </Grid>
           </Grid>
@@ -306,15 +355,15 @@ export default function ProntuarioCadastro() {
         <Grid
           item
           xs={12}
-          style={{ display: "flex", justifyContent: "flex-start" }}
+          style={{ display: 'flex', justifyContent: 'flex-start' }}
         >
-          <Typography variant="h5" display="block" gutterBottom>
+          <Typography variant='h5' display='block' gutterBottom>
             {dataExt}
           </Typography>
         </Grid>
         <Grid container spacing={3} style={{ marginTop: 20 }}>
           <Grid item xs={12}>
-            <div className="divEditor">
+            <div className='divEditor'>
               <header className={classes.texteditor}>Prontuário</header>
               <Editor
                 editorState={editorState}
@@ -326,12 +375,12 @@ export default function ProntuarioCadastro() {
             </div>
           </Grid>
         </Grid>
-        <Grid container spacing={3} style={{ marginTop: "30px" }}>
+        <Grid container spacing={3} style={{ marginTop: '30px' }}>
           <Grid item xs={6}>
             <Button
-              variant="contained"
-              color="#e8eaf6"
-              size="large"
+              variant='contained'
+              color='#e8eaf6'
+              size='large'
               startIcon={<SaveIcon />}
               onClick={SalvarProntuario}
             >
@@ -340,9 +389,9 @@ export default function ProntuarioCadastro() {
           </Grid>
           <Grid item xs={6}>
             <Button
-              variant="contained"
-              color="#e8eaf6"
-              size="large"
+              variant='contained'
+              color='#e8eaf6'
+              size='large'
               startIcon={<CancelPresentationIcon />}
               onClick={ClearFields}
             >
@@ -352,5 +401,5 @@ export default function ProntuarioCadastro() {
         </Grid>
       </div>
     </React.Fragment>
-  );
+  )
 }
