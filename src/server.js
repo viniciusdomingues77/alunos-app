cd03d5173d7f04ffb39e4d59759fde08674d30cd
Vinicius da Silva Domingues <i.niv2010@hotmail.com>
2021-07-16 20:09:02 -0300

    apontando para a vps

--- src/server.js
+++ src/server.js
@@ -1,2 +1,3 @@
-export const server = 'https://localhost:44363'
+//export const server = 'https://localhost:44363'
 //export const server = "http://vinidom-001-site1.dtempurl.com";
+export const server = 'http://localhost:5000'
