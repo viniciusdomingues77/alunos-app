9f3d26563e43071a015f999d73f3700c2f630b24
Vinicius da Silva Domingues <i.niv2010@hotmail.com>
2021-07-26 18:17:06 -0300

    testando exceção

--- src/server.js
+++ src/server.js
@@ -1,3 +1,3 @@
-export const server = "https://localhost:44363";
+//export const server = 'https://localhost:44363'
 //export const server = "http://vinidom-001-site1.dtempurl.com";
-//export const server = "http://5.183.11.111:81";
+export const server = 'http://5.183.11.111:81'
